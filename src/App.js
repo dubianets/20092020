9379c3e828062128a02dea3ae83cf0e33c8d3e89
dubianets <vadikdubianets@gmail.com>
2020-09-20 22:47:51 -0700

    change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ButtonPanel from "./ButtonPanel";
+import ControlPanel from "./ControlPanel";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counters, setCounters] = useState([0]);
+
+    const addCounter = (value) => {
+        const arrNewCounter = [...counters];
+        value = value  ? value : 0;
+        arrNewCounter.push(value);
+        setCounters(arrNewCounter);
+    }
+
+    const removeCounter = (index) => {
+        const arrRemove = counters.filter((el, i) =>{
+            return index !== i ? true : false; }
+        )
+        setCounters(arrRemove);
+    }
+
+    const changeValue = (index, val) => {
+        const arrChange = counters.map((el, i) => {
+            if (index === i) return el + val;
+            return el;
+        })
+        setCounters(arrChange);
+    }
+
+    const refreshAll = () => {
+        const arrRefresh = counters.map( el => el = 0);
+        setCounters(arrRefresh);
+     }
+
+    return (
+
+        <div>
+
+            <ControlPanel refreshAll={refreshAll} addCounter={addCounter}/>
+            <hr/>
+
+            <div>
+                {counters.map((el, index) => <ButtonPanel changeValue={changeValue} addCounter={addCounter} el={el}
+                                                          removeCounter={removeCounter}  index={index} key={Math.random()}/>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
